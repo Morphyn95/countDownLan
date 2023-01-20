200a76831d26a94148a42910e412c37d2e18d807
Morphyn95 <alexandre.deslypper@gmail.com>
2023-01-20 15:33:41 +0100

    V4

--- main.js
+++ main.js
@@ -17,8 +17,24 @@ var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Display the result in the element with id="demo"
-document.getElementById("demo").innerHTML = days + "d " + hours + "h "
-+ minutes + "m " + seconds + "s ";
+
+if (days< 10) {
+days =  `0 ${days}`}
+
+if (hours < 10) {
+hours = "0" + hours;}
+
+if (minutes < 10) {
+minutes = "0" + minutes;}
+
+if (seconds < 10) {
+seconds = "0" + seconds;}
+
+document.getElementById("days").innerHTML = days 
+document.getElementById("hours").innerHTML = hours 
+document.getElementById("minutes").innerHTML = minutes 
+document.getElementById("seconds").innerHTML = seconds;
+    
 
   // If the count down is finished, write some text
 if (distance < 0) {
